227e7ba1c40003919afd40edc0d87496d38f2104
dr.xiong.wen.hao <1277529106@qq.com>
2024-08-09 20:09:26 +0800

    New Code

--- _5weibo/src/main/java/cn/tedu/_5weibo/controller/UserController.java
+++ _5weibo/src/main/java/cn/tedu/_5weibo/controller/UserController.java
@@ -6,21 +6,27 @@ import cn.tedu._5weibo.pojo.dto.UserRegDTO;
 import cn.tedu._5weibo.pojo.vo.UserRegVO;
 import cn.tedu._5weibo.pojo.vo.UserSelectUserVO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpSession;
 import java.util.Date;
 import java.util.List;
 
 @RestController
 @RequestMapping("/v1/users/")
 public class UserController {
-    @Autowired
     private UserMapper mapper;
 
+    @Autowired
+    public void setMapper(UserMapper mapper) {
+        this.mapper = mapper;
+    }
+
     @PostMapping("reg")
-    public String userReg(UserRegDTO dto) {
+    public String userReg(UserRegDTO dto, HttpSession session) {
         String username = dto.getUsername();
         String password = dto.getPassword();
         String nickname = dto.getNickname();
@@ -41,6 +47,7 @@ public class UserController {
                 vo.setNickname(nickname);
                 vo.setCreated(new Date());
                 mapper.insertUser(vo);
+                session.setAttribute("user", username);
                 return "注册成功";
             } else {
                 System.out.println("用户重复");
@@ -50,12 +57,13 @@ public class UserController {
     }
 
     @PostMapping("login")
-    public String userLogin(UserLoginDTO dto) {
+    public String userCurrent(UserLoginDTO dto, HttpSession session) {
         String username = dto.getUsername();
         String password = dto.getPassword();
         List<UserSelectUserVO> user = mapper.selectNumByUsername(username);
         if (user.size() == 1) {
             if (user.get(0).getPassword().equals(password)) {
+                session.setAttribute("user", username);
                 return "登录成功";
             } else {
                 return "密码错误";
@@ -65,6 +73,38 @@ public class UserController {
         } else {
             System.out.println("用户重复");
             return "系统错误";
+        }
+    }
+
+    @GetMapping("current")
+    public String userCurrent(HttpSession session) {
+        String user = (String) session.getAttribute("user");
+        if (user == null) {
+            return "用户未登录";
+        } else {
+            return user + "登录中";
+        }
+    }
+
+    @GetMapping("outUser")
+    public String outUser(HttpSession session) {
+        if (session.getAttribute("user") == null) {
+            return "用户未登录";
+        } else {
+            session.removeAttribute("user");
+            return "已退出登录";
+        }
+    }
+
+    @PostMapping("delete")
+    public String deleteUser(HttpSession session) {
+        String user = (String) session.getAttribute("user");
+        if (user == null) {
+            return "未登录";
+        } else {
+            mapper.deleteUser(user);
+            session.removeAttribute("user");
+            return "已注销账户并退出登录";
         }
     }
 
--- _5weibo/src/main/java/cn/tedu/_5weibo/mapper/UserMapper.java
+++ _5weibo/src/main/java/cn/tedu/_5weibo/mapper/UserMapper.java
@@ -10,5 +10,5 @@ public interface UserMapper {
 
     void insertUser(UserRegVO vo);
 
-
+    void deleteUser(String username);
 }
